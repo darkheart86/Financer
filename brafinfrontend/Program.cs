018d26e7f93287211070f93e8b575941499c99fb
Heitor <heitormassarelli@gmail.com>
2017-05-18 21:52:34 -0300

    2

--- brafinfrontend/Program.cs
+++ brafinfrontend/Program.cs
@@ -18,6 +18,8 @@ namespace brafinfrontend
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
+           //ota fumiga
+
             
         }
     }
